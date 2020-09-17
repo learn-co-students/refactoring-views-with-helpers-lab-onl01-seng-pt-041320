3cfae682205bcdef1626d5edbefa632ea2b63ab2
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-16 21:44:37 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+    def display_artist(artist)
+        if artist.empty?
+            content_for :artist, artist
+        else
+            content_for :edit, artist
+        end
+    end
+
 end
