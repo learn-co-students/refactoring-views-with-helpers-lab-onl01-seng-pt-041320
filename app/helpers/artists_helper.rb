e9f925d993a25eb8c9b3a38251d767f0512b0218
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-17 17:28:09 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
-    def display_artist(artist)
-        if artist.empty?
-            artist.artist_name
+    def display_artist(song)
+        if song.artist && song.artist.name != ""
+            link_to song.artist.name, artist_path(song.artist)
         else
-            Song.artist_name=(artist)
+            link_to "Add Artist", edit_song_path(song)
         end
     end
 
